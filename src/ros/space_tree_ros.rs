dee0d5e673718ae9b1767c1530c63c920bff9aeb
endre90 <endre90@gmail.com>
2025-02-27 15:06:15 +0100

    trying volatile qos for tf sub 2

--- src/ros/space_tree_ros.rs
+++ src/ros/space_tree_ros.rs
@@ -86,7 +86,7 @@ impl RosSpaceTreeServer {
             .lock()
             .unwrap().create_publisher::<TFMessage>(
                 "tf",
-                QosProfile::volatile(QosProfile::default()),
+                QosProfile::transient_local(QosProfile::default()),
             ).expect("Failed to initialize active_frame_broadcaster.");
 
         let local_buffer_clone = local_buffer.clone();
@@ -119,7 +119,7 @@ impl RosSpaceTreeServer {
         });
 
         let static_tf_listener =
-            node.lock().unwrap().subscribe::<TFMessage>("tf_static", QosProfile::transient_local(QosProfile::default())).expect("Failed to initialize static_tf_listener.");
+            node.lock().unwrap().subscribe::<TFMessage>("tf_static", QosProfile::volatile(QosProfile::default())).expect("Failed to initialize static_tf_listener.");
         let global_buffer_clone = global_buffer.clone();
         tokio::task::spawn(async move {
             match static_tf_listener_callback(
